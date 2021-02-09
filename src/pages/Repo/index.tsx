b722aec328f89a3ffdfc0fb4580e02110c807ed0
Josephpaz <josephpazjj@gmail.com>
2021-02-09 13:15:47 -0400

    Added Repositorie Component

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react';
+import React from "react";
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
+
+import {Link} from 'react-router-dom';
 
 const Repo: React.FC = () => {
-    return <div/>;
+  return (
+    <Container>
+      <Breadcrumb>
+        <RepoIcon />
+        <Link className={"username"} to={"/josephpaz"}>
+          josephpaz
+        </Link>
+        <span>/</span>
+        <Link className={"repoName"} to={"/josephpaz/crud-laravel"}>
+          crud-laravel
+        </Link>
+      </Breadcrumb>
+      <p>Contains all of my Youtuebe lessons code.</p>
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>starts</span>
+        </li>
+        <li>
+          <ForkIcon />
+          <b>8</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+      <LinkButton href={"https://github.com/Josephpaz/crud-laravel"}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
-export default Repo; 
+export default Repo;
